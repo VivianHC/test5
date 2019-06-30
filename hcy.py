d6bb5e5573df0d983d0bb8918dcf4b8f9659c8d3
VivianHC <52123322+VivianHC@users.noreply.github.com>
2019-06-30 12:24:14 +0800

    第二次非常顺利？？？

---
+++ hcy.py
@@ -0,0 +1,12 @@
+# 练习二
+print("-----华丽分割线-----")
+s = input("请任意输入一串英语句子：")
+print(s.islower()) # 判断所有字符都是小写
+
+# print(s.isalnum()) # 判断所有字符都是数字或者字母
+# print(s.isalpha()) # 判断所有字符都是字母
+# print(s.isdigit()) # 判断所有字符都是数字
+# print(s.islower()) # 判断所有字符都是小写
+# print(s.isupper()) # 判断所有字符都是大写
+# print(s.istitle()) # 判断所有单词都是首字母大写，像标题
+# print(s.isspace()) # 判断所有字符都是空白字符、\t、\n、\r
